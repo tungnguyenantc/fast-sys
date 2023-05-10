66ab10cf8f18c931d79eb4a311ed8e04afcc9e4a
tungnguyenantc <tungnguyen.antc@gmail.com>
2023-05-10 21:25:19 +0700

    initial

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 import python_rust
 
 app = FastAPI()
-app.mount('/static', StaticFiles(directory='static'), name='static')
+# app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 SECRECT_KEY = '69906dd7545790cfbc6dfee552a537f44e1ddcf87f16537490a8e5520c256177'
